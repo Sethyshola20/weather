fc66fa5caf108c9b3f6bdba8d4373fa556daffb8
Sethyshola20 <sethylunelo@gmail.com>
2022-12-16 12:08:38 +0100

    implementing client side data fetching

--- app/component/cityForm.tsx
+++ app/component/cityForm.tsx
@@ -1,11 +1,41 @@
 "use client";
 
+import { WeatherData } from "../interface/WeatherData";
+import { useState } from "react";
+
 const CityForm = () => {
+  const [city, setCity] = useState("");
+
+  const getData = async () => {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}&aqi=yes`;
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const res = await fetch(url, options).then((res) => res.json());
+    if (!res.ok) {
+      throw new Error("Fetched did not work");
+    } else {
+      const data = res.json() as WeatherData;
+      const cityName = data.location.name;
+      setCity(cityName);
+    }
+  };
   return (
-    <form>
-      <input type="text" id="city" placeholder="enter your city" />
-      <button type="button">Submit</button>
-    </form>
+    <>
+      <form onSubmit={getData}>
+        <input
+          type="text"
+          id="city"
+          placeholder="enter your city"
+          name="city"
+        />
+        <button type="button">Submit</button>
+      </form>
+      <span>{city}</span>
+    </>
   );
 };
 export default CityForm;
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,9 @@
 import CityForm from "./component/cityForm";
-import { WeatherData } from "./interface/WeatherData";
 
-type param = {
-  params: {
-    city: string;
-  };
-};
-
-const getData = async (city: string) => {
-  const url = `https://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${city}&aqi=yes`;
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  const res = await fetch(url, options).then((res) => res.json());
-  return res.json() as WeatherData;
-};
-
-const handleclick = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  const town = new FormData(e.target as HTMLFormElement);
-  console.log(town);
-};
-
-const Home = async ({ params: { city } }: param) => {
-  const data = getData(city);
-
-  const [weatherData] = await Promise.all([data]);
-
+const Home = async () => {
   return (
     <main>
       <h1>See the latest weather</h1>
-      <div>{weatherData.location.name}</div>
       <CityForm />
     </main>
   );
