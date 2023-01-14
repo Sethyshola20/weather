3704ae051d87e5c67290a20e70dcdac9bc550f3f
Sethyshola20 <sethylunelo@gmail.com>
2023-01-14 18:46:55 +0100

    api now good, is it persistent?

--- pages/api/towns/[townName].ts
+++ pages/api/towns/[townName].ts
@@ -1,10 +1,11 @@
 import { NextApiResponse, NextApiRequest } from 'next';
+import { towns } from '.';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         if (req.method === 'GET') {
             const name = req.query.townName as string;
-            res.status(200).json({ name });
+            res.status(200).json(towns.includes(name) ? name : "There is no such town");
         }
     } catch (error) {
         console.error(error);
--- pages/api/towns/index.ts
+++ pages/api/towns/index.ts
@@ -1,14 +1,17 @@
 import { NextApiResponse, NextApiRequest } from 'next';
 
+export let towns = ["Montpellier", "Paris", "Marseille", "Lyon"]
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         if (typeof req.body !== 'string') {
             res.status(400).json({ error: 'Request body must be a string' });
         } else {
-            res.status(200).json({ message: 'well done' });
+            towns.push(req.body)
+            res.status(200).json({ message: 'well done', towns });
         }
     } else if (req.method === 'GET') {
-        res.status(200).json({});
+        res.status(200).json(towns);
     }
 };
 
