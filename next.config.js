db43f8f2e221581442abce2af8fb83ebf05f8c33
Sethyshola20 <sethylunelo@gmail.com>
2023-05-23 23:07:50 +0200

    next update

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https://",
-        hostname: "cdn.weatherapi.com",
-        port: "3000",
-        pathname: "/account123/**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
