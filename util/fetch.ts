41fa0e3f5c53be9d1b714eda04415080ef043d61
Sethyshola20 <sethylunelo@gmail.com>
2023-05-23 23:19:26 +0200

    options

--- util/fetch.ts
+++ util/fetch.ts
@@ -1,9 +1,17 @@
 export const fetchMyApiForTownArray = async (): Promise<string[]> => {
     // const url = "http://localhost:3000/api/towns";
     const url = "https://weather-time-two.vercel.app/api/towns";
+    const options = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "GET",
+        next: { revalidate: 0 },
+    };
 
     try {
-        const res = await fetch(url, { cache: "no-store" });
+
+        const res = await fetch(url, options);
         const data = await res.json();
         return data as string[];
     } catch {
